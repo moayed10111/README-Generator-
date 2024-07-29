ac0625d09a8a0b10ea7dbf16204d81c0f94d1a25
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-07-29 18:03:43 -0400

    adding the logic to my app

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
+import generateMarkdown from "../utils/generateMarkdown.js";
 // TODO: Create an array of questions for user input
 
 const questions = [
   inquirer
     .prompt([
       {
-        type: "",
+        type: "input",
         name: "title",
         message: "What is the name of your project ?",
       },
@@ -32,9 +33,9 @@ const questions = [
         message: "What did you learn?",
       },
       {
-        type:"input",
+        type: "input",
         name: "install",
-        message:"What are the steps required to install your project?"
+        message: "What are the steps required to install your project?",
       },
       {
         type: "input",
@@ -52,41 +53,29 @@ const questions = [
         message:
           "list the name of the collaborators with link to thier GitHub profiles",
       },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email"
+      },{
+        type:"input",
+        name:"gitHub",
+        message:"Please add your gitHub Username"
+      },
+      {
+        type: "list",
+        name: "badge",
+        message:
+          "Please enter the name of the programing language you used for this project",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "Mozilla", "None"],
+      },
     ])
     .then((readmeContent) => {
-      let readMe = `
-# ${readmeContent.title}
-    
-## Description
- ${readmeContent.motivation},${readmeContent.reason},${readmeContent.solved},${readmeContent.learn}
-
- ## Installation 
- ${readmeContent.install}
-
-## Usage
- ${readmeContent.usage}
-
-## Features
-${readmeContent.features}
-
-## Credits
-${readmeContent.credits}`;
-
-      fs.writeFile("./README/README.md", readMe, (err) =>
-        err ? console.error(err) : console.log("saved")
+      fs.writeFile(
+        "./README/README.md",
+        generateMarkdown(readmeContent),
+        (err) => (err ? console.error(err) : console.log("saved"))
       );
     }),
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-  // questions();
-  // writeToFile();
-}
-
-// Function call to initialize app
-
-init();
